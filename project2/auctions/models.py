11f2d6b9ebe9a55960a5ec8c47f79e14bc7a8bd0
Nikcio <nikolajgive@gmail.com>
2020-09-15 10:53:53 +0200

    models update

--- project2/auctions/models.py
+++ project2/auctions/models.py
@@ -3,4 +3,26 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
+class Listing(models.Model):
+    image = models.ImageField()
+    description = models.CharField(max_length=1084)
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_date = models.DateTimeField(auto_now_add=True)
+    close_date = models.DateTimeField()
+
+
+class Bid(models.Model):
+    amount = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=1084)
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
